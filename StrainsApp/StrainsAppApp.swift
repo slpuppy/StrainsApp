95b4f64f76f2028294fedc8ec897942a54957f3a
Gabriel Puppi <puppizin@gmail.com>
2021-05-28 09:01:09 -0300

    Initial Commit

---
+++ StrainsApp/StrainsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrainsAppApp.swift
+//  StrainsApp
+//
+//  Created by Gabriel Puppi on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct StrainsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
